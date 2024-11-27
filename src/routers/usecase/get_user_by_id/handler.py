5fcceaf17817321da8a1a4d7dca8d29ea237f2d1
Adarsh Ray <adarshray39@gmail.com>
2024-11-27 17:29:09 +0530

    Your commit message

--- src/routers/usecase/get_user_by_id/handler.py
+++ src/routers/usecase/get_user_by_id/handler.py
@@ -1,16 +1,20 @@
-import flask
 from flask import Request
 
 from src.routers.base_handler import BaseHandler, BaseResponse, BaseRequest
 from src.entity import User
 from src.service import get_user_service
-
+from .response import GetUserByIdResponse
 from .request import GetUserByIdRequest
 
 class GetUserByIdHandler(BaseHandler):
     def get_request(self, req: Request) -> BaseRequest:
         return GetUserByIdRequest(req)
 
-    def handle_request(self, req: BaseRequest) -> BaseResponse:
-        user_id =
-        user = get_user_service().get_user_by_id(req)
+    def handle_request(self, req: GetUserByIdRequest) -> BaseResponse:
+        ID = req.user_id
+        user = get_user_service().get_user_by_id(ID)
+        return GetUserByIdResponse(user)
+
+
+def get_handler() -> BaseHandler:
+    return GetUserByIdHandler()
